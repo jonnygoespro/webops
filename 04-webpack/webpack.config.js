53098e6949a3906db145029b57c51552e462b5db
Jonas Buggisch <jonas.buggisch@gmail.com>
2022-11-18 10:14:53 +0100

    Styling moved to js file

---
+++ 04-webpack/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+        ],
+      },
+    ],
+  },
+};
